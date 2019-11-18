ccd1ea56f1ac6500e20fd36d299e44111eb5a64d
Tery Lim <kumabotz@gmail.com>
2019-11-18 20:23:16 +1300

    Initial commit

---
+++ Assets/Scripts/HellowWorld.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class CustomDataStructure
+{
+    public string name;
+    public GameObject target;
+    public Vector3 position;
+}
+
+public class HellowWorld : MonoBehaviour
+{
+    [Range(0f, 10f)]
+    public float speed;
+    public Vector3 startPos;
+
+    [Tooltip("What the GameObject should follow")]
+    public GameObject target;
+    public string message = "Hello World";
+
+    [Header("Array Tests")]
+    public string[] strings;
+    public int[] numbers;
+    public Vector3[] positions;
+    public GameObject[] gameObjects;
+
+    public CustomDataStructure[] customTarget;
+
+    [HideInInspector]
+    public List<string> stringList;
+
+    [Header("Dictionary and Property Tests")]
+    public Dictionary<string, Vector3> childrenPositions;
+    public int life { get; set; }
+}
